9f7aef652bd6894d3b19afea2a0f1b25e165b71a
evanchen2023 <133299504+evanchen2023@users.noreply.github.com>
2025-04-08 22:54:14 +1200

    Updated AuthForm page
    1. + authFormSchema function
    2. + link to sign in or sign up page
    3. * submit handler function

---
+++ components/Formfield.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
+import {Input} from "@/components/ui/input";
+
+const Formfield = () => (
+    <FormField
+        control={form.control}
+        name="username"
+        render={({ field }) => (
+            <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                    <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                    This is your public display name.
+                </FormDescription>
+                <FormMessage />
+            </FormItem>
+        )}
+    />
+);
+
+export default Formfield;
